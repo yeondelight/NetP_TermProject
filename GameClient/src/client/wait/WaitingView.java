a2c46ae78a1ccf709982b54de939f31cb718c373
yeondelight <hihi5v@hansung.ac.kr>
2021-10-26 21:58:10 +0900

    add GameServer.java & loginView

--- GameClient/src/client/wait/WaitingView.java
+++ GameClient/src/client/wait/WaitingView.java
@@ -6,6 +6,13 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
 import java.util.HashMap;
 
 import javax.swing.ImageIcon;
@@ -15,6 +22,18 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 
 public class WaitingView extends JFrame{
+	
+	// 서버에게 전송하기 위한 코드. 항상 "코드 메세지"의 형태로 전송할것.
+	private static final int C_LOGIN = 100;		// 로그인시 서버에 전송하는 코드 : 100
+	
+	
+	private static final int BUF_LEN = 128; //  Windows 처럼 BUF_LEN 을 정의
+	private Socket socket; // 연결소켓
+	private InputStream is;
+	private OutputStream os;
+	private DataInputStream dis;
+	private DataOutputStream dos;
+	private String userName;
 	
 	private Container contentPane;
 	private JScrollPane scrollPane;
@@ -30,8 +49,9 @@ public class WaitingView extends JFrame{
 	private JLabel imgLabel = new JLabel(img);
 	
 	// JFrame 생성 : Swing Frame
-	public WaitingView() {
-		
+	public WaitingView(String username, String ip_addr, String port_no) {
+		this.userName = username;
+
 		// 기본 설정
 		setTitle("Network MAZE Game - Waiting Room");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -84,5 +104,101 @@ public class WaitingView extends JFrame{
 		// focus 지정 - Mouse Listener를 받을 수 있게 함
 		contentPane.setFocusable(true);
 		contentPane.requestFocus();
+		
+		// client codes
+		try {
+			socket = new Socket(ip_addr, Integer.parseInt(port_no));
+			is = socket.getInputStream();
+			dis = new DataInputStream(is);
+			os = socket.getOutputStream();
+			dos = new DataOutputStream(os);
+			
+			SendMessage(C_LOGIN + " " + userName);		// 로그인 정보를 서버에게 전송
+			ListenNetwork net = new ListenNetwork();
+			net.start();
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("connect error");
+		}
+	}
+	
+	// Server Message를 수신해서 화면에 표시
+	class ListenNetwork extends Thread {
+		public void run() {
+			while (true) {
+				try {
+					// String msg = dis.readUTF();
+					byte[] b = new byte[BUF_LEN];
+					int ret;
+					ret = dis.read(b);
+					if (ret < 0) {
+						System.out.println("dis.read() < 0 error");
+						try {
+							dos.close();
+							dis.close();
+							socket.close();
+							break;
+						} catch (Exception ee) {
+							break;
+						}// catch문 끝
+					}
+					String	msg = new String(b, "euc-kr");
+					msg = msg.trim(); // 앞뒤 blank NULL, \n 모두 제거
+					System.out.println(msg);
+				} catch (IOException e) {
+					System.out.println("dis.read() error");
+					try {
+						dos.close();
+						dis.close();
+						socket.close();
+						break;
+					} catch (Exception ee) {
+						break;
+					} // catch문 끝
+				} // 바깥 catch문끝
+				
+			}
+		}
+	}
+	
+	// Windows 처럼 message 제외한 나머지 부분은 NULL 로 만들기 위한 함수
+	public byte[] MakePacket(String msg) {
+		byte[] packet = new byte[BUF_LEN];
+		byte[] bb = null;
+		int i;
+		for (i = 0; i < BUF_LEN; i++)
+			packet[i] = 0;
+		try {
+			bb = msg.getBytes("euc-kr");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(0);
+		}
+		for (i = 0; i < bb.length; i++)
+			packet[i] = bb[i];
+		return packet;
+	}
+
+	// Server에게 network으로 전송
+	public void SendMessage(String msg) {
+		try {
+			// dos.writeUTF(msg);
+			byte[] bb;
+			bb = MakePacket(msg);
+			dos.write(bb, 0, bb.length);
+		} catch (IOException e) {
+			System.out.println("dos.write() error");
+			try {
+				dos.close();
+				dis.close();
+				socket.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				System.exit(0);
+			}
+		}
 	}
 }
--- GameServer/src/server/main/MainApp.java
+++ GameServer/src/server/main/MainApp.java
@@ -1,17 +1,14 @@
-package client.main;
+package server.main;
 
 import java.awt.EventQueue;
 
 public class MainApp {
-	
-	/**
-	 * Launch the application.
-	 */
+	// Launch the application.
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GameClient frame = new GameClient();
+					GameServer frame = new GameServer();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
